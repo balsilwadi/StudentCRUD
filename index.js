c8ce51b59bfaeb262d8dfffdb4844fd56000dc6b
Bilal Alsilwadi <balsilwadi@gmail.com>
2023-06-16 18:03:57 -0500

    added index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-// routes
-// mostly work with JSON at the beginning
 
-// Render home page
 app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-
-const userRouter = require('./routes/student.js')
+const userRouter = require('./routes/students.js')
 app.use('/api/students', userRouter)
 
 app.listen(5000, () => console.log("Server running on port 5000"));
